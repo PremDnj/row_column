ed21a29c6353c9028ce11d408f2dcd1ffc30fd55
PremDnj <prem.danej@dhiwise.com>
2022-09-27 16:57:30 +0530

    Second  Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:row_column/main.dart';
+import 'package:row_column/presentation/awesome_app_screen/awesome_app_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
